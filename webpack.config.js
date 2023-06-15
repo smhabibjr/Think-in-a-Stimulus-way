549d1e23352348366d06f61ebf11508ed1ec1af3
HabibJR <smhabib.abir2030@gmail.com>
2023-06-15 23:10:13 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    bundle: "./src/index.js"
+  },
+
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public")
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/
+        ],
+        use: [
+          { loader: "babel-loader" }
+        ]
+      }
+    ]
+  }
+};
